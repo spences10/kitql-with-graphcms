994afe22c881c8449f7729247d9f60a5c6b78041
spences10 <spences10apps@gmail.com>
2022-02-22 17:50:02 +0000

    chore: :wrench: same origin credentials

--- src/lib/graphql/kitQLClient.ts
+++ src/lib/graphql/kitQLClient.ts
@@ -2,7 +2,7 @@ import { KitQLClient } from '@kitql/client'
 
 export const kitQLClient = new KitQLClient({
   url: 'https://api-eu-central-1.graphcms.com/v2/cktod9uc411qp01yz80gwh9bo/master',
-  credentials: 'omit',
+  credentials: 'same-origin',
   defaultCache: 1000 * 60 * 3,
   headersContentType: 'application/json',
   logType: ['client', 'server', 'operationAndvariables'],
